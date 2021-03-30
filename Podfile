6465577b845b3db1c94ab596cb90eb9c52669944
chengmin-z <chengmin.z@outlook.com>
2021-03-30 10:34:37 +0800

    podfile and generate xcworkspace

---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'writeHelper' do
+  pod 'ReactiveObjC' , '~> 3.1.1'
+  pod 'SVProgressHUD' , '~> 2.2.5'
+  pod 'MJRefresh' , '~> 3.5.0'
+  pod 'SDWebImage' , '~> 5.10.4'
+  pod 'Masonry' , '~> 1.1.0'
+  pod 'IGListKit' , '~> 4.0.0'
+  pod 'OpenCV' , '~> 4.1.0'
+  pod 'Mantle' , '~> 2.1.6'
+  pod 'AFNetworking' , '~> 4.0.1'
+end
--- writeHelper/AppDelegate.m
+++ writeHelper/AppDelegate.m
@@ -16,7 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.rootViewController = [[UINavigationController alloc]init];
+    self.window.rootViewController = [[UITabBarController alloc]init];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
